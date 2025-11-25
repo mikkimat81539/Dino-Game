8dd155efe8e84b68245e054ac229b65ba7177ee0
Mikaela Matthews <mikaelamatthews@MacBookAir.lan>
2025-11-25 14:08:28 -0600

    made screen and game loop

---
+++ main.py
@@ -0,0 +1,19 @@
+import pygame, sys
+
+pygame.init()
+
+# SCREEN
+screen = pygame.display.set_mode((500, 250))
+pygame.display.set_caption("DINO GAME")
+
+# Game Loop
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+    
+    screen.fill("white")
+
+    # Render Code here
+    
+    pygame.display.flip()
