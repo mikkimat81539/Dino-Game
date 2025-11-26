e8847aad4bb903b9add888646924dd2d27445e78
Mikaela Matthews <mikaelamatthews@MacBookAir.lan>
2025-11-25 18:59:19 -0600

    created player movement

--- drawGame.py
+++ drawGame.py
@@ -2,7 +2,7 @@ import pygame
 
 pygame.init()
 
-class gamePlayDraw:
+class gamePlayDraw_Squares:
     def __init__(self, x_pos, y_pos, width, height, color):
         self.x_pos = x_pos
         self.y_pos = y_pos
@@ -10,6 +10,20 @@ class gamePlayDraw:
         self.height = height
         self.color = str(color)
         self.get_rect = pygame.Rect(self.x_pos, self.y_pos, self.width, self.height)
-    
+
     def draw_rect(self, screen):
         return pygame.draw.rect(screen, self.color, self.get_rect)
+
+class gamePlayDraw_Triange:
+    def __init__(self, Lx_pos, Ly_pos, Tx_pos, Ty_pos, Rx_pos, Ry_pos, color):
+        self.Lx_pos = Lx_pos # Left part of triange on X axis
+        self.Ly_pos = Ly_pos # Left part of triange on Y axis
+        self.Tx_pos = Tx_pos # Top part of triange on X axis
+        self.Ty_pos = Ty_pos # Top part of triange on Y axis
+        self.Rx_pos = Rx_pos # Right part of triange on X axis
+        self.Ry_pos = Ry_pos # Right part of triange on Y axis
+        self.set_points = [(self.Lx_pos, self.Ly_pos), (self.Tx_pos, self.Ty_pos), (self.Rx_pos, self.Ry_pos)]
+        self.color = str(color)
+    
+    def draw_polygon(self, screen):
+        return pygame.draw.polygon(screen, self.color, self.set_points)
--- main.py
+++ main.py
@@ -1,21 +1,49 @@
 import pygame, sys
+from drawGame import gamePlayDraw_Squares
 
 pygame.init()
+
+clock = pygame.time.Clock()
 
 # SCREEN
 screen = pygame.display.set_mode((500, 250))
 pygame.display.set_caption("DINO GAME")
 
 # PLAYER
+player = gamePlayDraw_Squares(10, 190, 30, 30, "green")
+
+# PLATFORM
+platform = gamePlayDraw_Squares(0, 220, 500, 30, "gray")
+
+# PHYSICS
+speed_x = 5
+speed_y = 30
 
 # Game Loop
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+        
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse = pygame.mouse.get_pos()
+            print(mouse)
     
+    # PLAYER MOVEMENT
+    player.get_rect.x += speed_x
+
+    keys = pygame.key.get_pressed()
+    
+    if keys[pygame.K_SPACE]:
+        player.get_rect.y -= speed_y
+
+
     screen.fill("white")
 
     # Render Code here
+    player.draw_rect(screen)
+    platform.draw_rect(screen)
     
     pygame.display.flip()
+    
+    clock.tick(35)
